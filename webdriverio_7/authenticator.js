6af86e90ef582628eeaed450fdd619094417cb18
Hristo G <hgenev@perforce.com>
2021-09-30 19:38:03 +0300

    Added keys example for emulating backspace, enter and tab keys

---
+++ webdriverio_7/authenticator.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const os = require('os')
+
+exports.getToken = function(cloud) {
+  console.log("Finding token for cloud " + cloud);
+  try
+  {
+    let path = process.env.PERFECTO_TOKEN_STORAGE
+    if (!path)
+      path = (os.platform() == 'win32') ? process.env.USERPROFILE + "\\securityTokens.json" : process.env.HOME + "/securityTokens.json";
+
+    console.log('Searching for token in: ' + path)  
+    let rawdata = fs.readFileSync(path);
+    let jsonData = JSON.parse(rawdata);
+
+    return jsonData.tokens[cloud];    
+  }
+  catch(err)
+  {
+    console.log(err)
+    return "NOT FOUND!"
+  }
+};
